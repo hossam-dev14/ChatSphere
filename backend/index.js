a9f0bf1906ce390d3a0b651d0b7a294b25cd759c
Muhammad Haroon <contactwithharoon@gmail.com>
2023-11-26 19:48:52 +0500

    basic chat app created with socket.io react and node,express

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const cors = require('cors');
+const http = require('http');
+const { Server } = require("socket.io");
+
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+io.on('connection', (socket)=>{
+    console.log(`User Connected : ${socket.id}`);
+
+    socket.on('sendMessage',(data)=>{
+        console.log(data);   
+        
+        io.emit('receiveMessage',data);
+    })
+
+    socket.on('disconnect',()=>{
+        console.log(`User Disconnected`);
+    })
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
